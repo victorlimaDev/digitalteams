63cc0c871a7335924852b8e9e6e7687cc76b4d8c
Victor Lima <vflcbrunello@gmail.com>
2024-01-30 12:12:35 -0300

    ouvindo o evento de criar

--- js/index.js
+++ js/index.js
@@ -4,3 +4,34 @@ criarBtn.onclick = () => {
     overlay.classList.add('show');
     formCriar.classList.add('show');
 }
+overlay.onclick = () => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+formFechar.onclick=() => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+formCriar.onsubmit = () => {
+    event.preventDefault();
+
+(nome.value+' '+capacidade.value);
+
+    listTeams.innerHTML = '';
+    listTeams.innerHTML = `
+    <li>
+         <h4>Nome do Team <box-icon name="show"></box-icon></h4>
+         <h1>0 <span>/ 0</span></h1>
+         <div class="actions">
+             <button>adicionar</button>
+             <button><box-icon name="trash"></box-icon></button>
+        </div>
+    </li>
+    `;
+
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+
